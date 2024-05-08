ae5e847d312d3c809333fc2999f259d3ae4a650a
gamesilva <gabrielmendes96@hotmail.com>
2024-05-07 21:00:47 -0300

    retornando com StreamLoginPresenter

--- lib/main/factories/pages/login/login_page_factory.dart
+++ lib/main/factories/pages/login/login_page_factory.dart
@@ -4,5 +4,5 @@ import '../../../../ui/pages/pages.dart';
 import '../../../factories/factories.dart';
 
 Widget makeLoginPage() {
-  return LoginPage(presenter: makeGetxLoginPresenter());
+  return LoginPage(presenter: makeStreamLoginPresenter());
 }
