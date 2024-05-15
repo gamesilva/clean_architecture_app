3777edef4eb1bfc7cbf97e979dd2d1a147dc4629
gamesilva <gabrielmendes96@hotmail.com>
2024-05-14 21:27:39 -0300

    ensure StreamSplashPresenter emits login page on empty results

--- test/presentation/presenters/stream_splash_presenter_test.dart
+++ test/presentation/presenters/stream_splash_presenter_test.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+import 'package:clean_architecture_app/domain/entities/account_entity.dart';
+import 'package:faker/faker.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
@@ -16,8 +18,8 @@ class StreamSplashPresenter implements SplashPresenter {
 
   @override
   Future<void>? checkAccount() async {
-    await loadCurrentAccount.load();
-    _navigateTo.add('/surveys');
+    final account = await loadCurrentAccount.load();
+    _navigateTo.add(account != null ? '/surveys' : '/login');
   }
 }
 
@@ -27,20 +29,33 @@ void main() {
   late LoadCurrentAccount loadCurrentAccount;
   late SplashPresenter sut;
 
+  void mockLoadCurrentAccount({AccountEntity? account}) =>
+      when(() => loadCurrentAccount.load()).thenAnswer((_) async => account);
+
   setUp(() {
     loadCurrentAccount = LoadCurrentAccountSpy();
     sut = StreamSplashPresenter(loadCurrentAccount: loadCurrentAccount);
   });
 
   test('Should call LoadCurrentAccount', () async {
+    mockLoadCurrentAccount(account: AccountEntity(faker.guid.guid()));
     await sut.checkAccount();
 
     verify(() => loadCurrentAccount.load()).called(1);
   });
 
   test('Should go to surveys page on success', () async {
+    mockLoadCurrentAccount(account: AccountEntity(faker.guid.guid()));
     sut.navigateToStream
         .listen(expectAsync1((page) => expect(page, '/surveys')));
+
+    await sut.checkAccount();
+  });
+
+  test('Should go to login page on null result', () async {
+    mockLoadCurrentAccount(account: null);
+
+    sut.navigateToStream.listen(expectAsync1((page) => expect(page, '/login')));
 
     await sut.checkAccount();
   });
