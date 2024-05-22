3c5f63d0cb45fffddcdeee8f923a740d0fca8af7
gamesilva <gabrielmendes96@hotmail.com>
2024-05-21 22:01:32 -0300

    ensure SignUpPage calls Validation on any field change

--- lib/ui/pages/signup/components/email_input.dart
+++ lib/ui/pages/signup/components/email_input.dart
@@ -1,21 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import '../../../helpers/helpers.dart';
+import '../signup_presenter.dart';
 
 class EmailInput extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      decoration: InputDecoration(
-        labelText: R.strings.email,
-        labelStyle: TextStyle(
-          color: Theme.of(context).primaryColor,
-        ),
-        icon: Icon(
-          Icons.email,
-          color: Theme.of(context).primaryColorLight,
-        ),
-      ),
-      keyboardType: TextInputType.emailAddress,
+    final presenter = Provider.of<SignUpPresenter>(context);
+    return StreamBuilder<UIError?>(
+      stream: presenter.emailErrorStream,
+      builder: (context, snapshot) {
+        return TextFormField(
+          decoration: InputDecoration(
+            labelText: R.strings.email,
+            labelStyle: TextStyle(
+              color: Theme.of(context).primaryColor,
+            ),
+            icon: Icon(
+              Icons.email,
+              color: Theme.of(context).primaryColorLight,
+            ),
+          ),
+          onChanged: presenter.validateEmail,
+          keyboardType: TextInputType.emailAddress,
+        );
+      },
     );
   }
 }
--- lib/ui/pages/signup/signup_page.dart
+++ lib/ui/pages/signup/signup_page.dart
@@ -1,14 +1,17 @@
 // ignore_for_file: deprecated_member_use
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../components/components.dart';
 import '../../helpers/helpers.dart';
 
 import 'components/components.dart';
+import 'signup_presenter.dart';
 
 class SignUpPage extends StatefulWidget {
-  const SignUpPage({Key? key}) : super(key: key);
+  final SignUpPresenter? presenter;
+  SignUpPage(this.presenter);
 
   @override
   State<SignUpPage> createState() => _SignUpPage();
@@ -37,27 +40,31 @@ class _SignUpPage extends State<SignUpPage> {
                   Headline1(text: R.strings.addAccount),
                   Padding(
                     padding: const EdgeInsets.all(32),
-                    child: Form(
-                      child: Column(
-                        children: <Widget>[
-                          NameInput(),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 8.0),
-                            child: EmailInput(),
-                          ),
-                          PasswordInput(),
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(top: 8.0, bottom: 32),
-                            child: PasswordConfirmationInput(),
-                          ),
-                          SignUpButton(),
-                          FlatButton.icon(
-                            onPressed: () {},
-                            icon: const Icon(Icons.exit_to_app),
-                            label: Text(R.strings.login),
-                          )
-                        ],
+                    child: Provider(
+                      create: (_) => widget.presenter,
+                      child: Form(
+                        child: Column(
+                          children: <Widget>[
+                            NameInput(),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 8.0),
+                              child: EmailInput(),
+                            ),
+                            PasswordInput(),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 8.0, bottom: 32),
+                              child: PasswordConfirmationInput(),
+                            ),
+                            SignUpButton(),
+                            FlatButton.icon(
+                              onPressed: () {},
+                              icon: const Icon(Icons.exit_to_app),
+                              label: Text(R.strings.login),
+                            )
+                          ],
+                        ),
                       ),
                     ),
                   )
