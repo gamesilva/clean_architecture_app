b1446c382d2c74c74aca66f6b3e9b35183edcb8e
gamesilva <gabrielmendes96@hotmail.com>
2024-05-21 22:07:40 -0300

    ensure SignUpPage present email error

--- lib/ui/pages/signup/components/email_input.dart
+++ lib/ui/pages/signup/components/email_input.dart
@@ -14,6 +14,7 @@ class EmailInput extends StatelessWidget {
         return TextFormField(
           decoration: InputDecoration(
             labelText: R.strings.email,
+            errorText: snapshot.hasData ? snapshot.data?.description : null,
             labelStyle: TextStyle(
               color: Theme.of(context).primaryColor,
             ),
