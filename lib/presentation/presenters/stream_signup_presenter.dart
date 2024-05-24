5b6e2b5343c77f4ab6775fd2bf42fc84ef5b7ebf
gamesilva <gabrielmendes96@hotmail.com>
2024-05-24 19:38:37 -0300

    ensure StreamSignUpPresenter emits isFormValid true if all fields are valid

--- lib/presentation/presenters/stream_signup_presenter.dart
+++ lib/presentation/presenters/stream_signup_presenter.dart
@@ -8,8 +8,20 @@ class SignUpState {
   UIError? nameError;
   UIError? passwordError;
   UIError? passwordConfirmationError;
+  String? name;
+  String? email;
+  String? password;
+  String? passwordConfirmation;
 
-  bool get isFormValid => false;
+  bool get isFormValid =>
+      nameError == null &&
+      emailError == null &&
+      passwordError == null &&
+      passwordConfirmationError == null &&
+      name != null &&
+      email != null &&
+      password != null &&
+      passwordConfirmation != null;
 }
 
 class StreamSignUpPresenter {
@@ -46,21 +58,25 @@ class StreamSignUpPresenter {
   }
 
   void validateName(String name) {
+    _state.name = name;
     _state.nameError = _validateField(field: 'name', value: name);
     _update();
   }
 
   void validateEmail(String email) {
+    _state.email = email;
     _state.emailError = _validateField(field: 'email', value: email);
     _update();
   }
 
   void validatePassword(String password) {
+    _state.password = password;
     _state.passwordError = _validateField(field: 'password', value: password);
     _update();
   }
 
   void validatePasswordConfirmation(String passwordConfirmation) {
+    _state.passwordConfirmation = passwordConfirmation;
     _state.passwordConfirmationError = _validateField(
       field: 'passwordConfirmation',
       value: passwordConfirmation,
