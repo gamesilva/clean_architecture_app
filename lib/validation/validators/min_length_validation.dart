aa6c58aa7cb95380ca808ac99e18bb2b001b658b
gamesilva <gabrielmendes96@hotmail.com>
2024-05-27 20:57:18 -0300

    change LoginComposition to use MinLenghtValidation on password

--- lib/validation/validators/min_length_validation.dart
+++ lib/validation/validators/min_length_validation.dart
@@ -1,10 +1,15 @@
+import 'package:equatable/equatable.dart';
+
 import '../../presentation/protocols/protocols.dart';
 import '../protocols/protocols.dart';
 
-class MinLengthValidation implements FieldValidation {
+class MinLengthValidation extends Equatable implements FieldValidation {
   @override
   final String field;
   final int size;
+
+  @override
+  List get props => [field, size];
 
   MinLengthValidation({
     required this.field,
