717b4476b26994920732425e5f5b4e32ff883fa7
gamesilva <gabrielmendes96@hotmail.com>
2024-06-10 23:08:34 -0300

    move StreamSurveyPresenter to its own file

---
+++ lib/presentation/presenters/stream_surveys_presenter.dart
@@ -0,0 +1,52 @@
+import 'dart:async';
+
+import 'package:intl/intl.dart';
+
+import '../../domain/helpers/helpers.dart';
+import '../../domain/usecases/usecases.dart';
+
+import '../../ui/helpers/helpers.dart';
+import '../../ui/pages/pages.dart';
+
+class StreamSurveysPresenter {
+  final LoadSurveys loadSurveys;
+
+  final StreamController<bool> _isLoading = StreamController<bool>();
+  final StreamController<List<SurveyViewModel>> _surveys =
+      StreamController<List<SurveyViewModel>>();
+
+  Stream<bool> get isLoadingStream => _isLoading.stream;
+
+  Stream<List<SurveyViewModel>> get surveyStream => _surveys.stream;
+
+  StreamSurveysPresenter({required this.loadSurveys});
+
+  void _updateIsLoding(bool isLoading) {
+    _isLoading.add(isLoading);
+  }
+
+  void _updateSurveys(List<SurveyViewModel> surveys) {
+    _surveys.add(surveys);
+  }
+
+  Future<void>? loadData() async {
+    try {
+      _updateIsLoding(true);
+
+      final surveys = await loadSurveys.load();
+      final surveysViewModel = surveys!
+          .map((survey) => SurveyViewModel(
+                id: survey.id!,
+                question: survey.question!,
+                date: DateFormat('dd MMM yyyy').format(survey.dateTime!),
+                didAnswer: survey.didAnswer!,
+              ))
+          .toList();
+      _updateSurveys(surveysViewModel);
+    } on DomainError {
+      _surveys.addError(UIError.unexpected.description);
+    } finally {
+      _updateIsLoding(false);
+    }
+  }
+}
--- test/presentation/presenters/stream_surveys_presenter_test.dart
+++ test/presentation/presenters/stream_surveys_presenter_test.dart
@@ -1,63 +1,20 @@
-import 'dart:async';
 import 'package:clean_architecture_app/domain/helpers/helpers.dart';
 import 'package:clean_architecture_app/ui/helpers/helpers.dart';
 import 'package:faker/faker.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
-import 'package:intl/intl.dart';
 
 import 'package:clean_architecture_app/domain/entities/entities.dart';
 import 'package:clean_architecture_app/domain/usecases/usecases.dart';
 
+import 'package:clean_architecture_app/presentation/presenters/presenters.dart';
+
 import 'package:clean_architecture_app/ui/pages/pages.dart';
-
-class StreamSurveysPresenter {
-  final LoadSurveys loadSurveys;
-
-  final StreamController<bool> _isLoading = StreamController<bool>();
-  final StreamController<List<SurveyViewModel>> _surveys =
-      StreamController<List<SurveyViewModel>>();
-
-  Stream<bool> get isLoadingStream => _isLoading.stream;
-
-  Stream<List<SurveyViewModel>> get surveyStream => _surveys.stream;
-
-  StreamSurveysPresenter({required this.loadSurveys});
-
-  void _updateIsLoding(bool isLoading) {
-    _isLoading.add(isLoading);
-  }
-
-  void _updateSurveys(List<SurveyViewModel> surveys) {
-    _surveys.add(surveys);
-  }
-
-  Future<void>? loadData() async {
-    try {
-      _updateIsLoding(true);
-
-      final surveys = await loadSurveys.load();
-      final surveysViewModel = surveys!
-          .map((survey) => SurveyViewModel(
-                id: survey.id!,
-                question: survey.question!,
-                date: DateFormat('dd MMM yyyy').format(survey.dateTime!),
-                didAnswer: survey.didAnswer!,
-              ))
-          .toList();
-      _updateSurveys(surveysViewModel);
-    } on DomainError {
-      _surveys.addError(UIError.unexpected.description);
-    } finally {
-      _updateIsLoding(false);
-    }
-  }
-}
 
 class LoadSurveysSpy extends Mock implements LoadSurveys {}
 
 void main() {
-  late LoadSurveysSpy loadSurveys;
+  late LoadSurveys loadSurveys;
   late StreamSurveysPresenter sut;
   late List<SurveyEntity> surveys;
 
