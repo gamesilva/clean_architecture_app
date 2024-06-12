f75a3950a75529bb391fdadf349b875687148e5a
gamesilva <gabrielmendes96@hotmail.com>
2024-06-11 22:05:58 -0300

    ensure Decorator throws ForbiddenError if FetchSecureCacheStorage throws

--- test/main/decorators/authorize_http_client_decorator_test.dart
+++ test/main/decorators/authorize_http_client_decorator_test.dart
@@ -21,15 +21,19 @@ class AuthorizeHttpClientDecorator implements HttpClient {
     Map? body,
     Map? headers,
   }) async {
-    final token = await fetchSecureCacheStorage.fetchSecure('token');
-    final authorizedHeaders = headers ?? {}
-      ..addAll({'x-access-key': token});
-    return await decoratee.request(
-      url: url,
-      method: method,
-      body: body,
-      headers: authorizedHeaders,
-    );
+    try {
+      final token = await fetchSecureCacheStorage.fetchSecure('token');
+      final authorizedHeaders = headers ?? {}
+        ..addAll({'x-access-key': token});
+      return await decoratee.request(
+        url: url,
+        method: method,
+        body: body,
+        headers: authorizedHeaders,
+      );
+    } catch (error) {
+      throw HttpError.forbidden;
+    }
   }
 }
 
@@ -49,10 +53,17 @@ void main() {
   late String token;
   late String httpResponse;
 
+  When mockTokenCall() =>
+      when(() => fetchSecureCacheStorage.fetchSecure(any()));
+
   void mockToken() {
     token = faker.guid.guid();
-    when(() => fetchSecureCacheStorage.fetchSecure(any()))
-        .thenAnswer((_) async => token);
+    mockTokenCall().thenAnswer((_) async => token);
+  }
+
+  void mockTokenError() {
+    token = faker.guid.guid();
+    mockTokenCall().thenThrow((_) async => Exception());
   }
 
   void mockHttpResponse() {
@@ -113,5 +124,13 @@ void main() {
     final response = await sut.request(url: url, method: method, body: body);
 
     expect(response, httpResponse);
+  });
+
+  test('Should throw ForbiddenError if FetchSecureCacheStorage throws',
+      () async {
+    mockTokenError();
+    final future = sut.request(url: url, method: method, body: body);
+
+    expect(future, throwsA(HttpError.forbidden));
   });
 }
