a43e2af3a40dee12519ff36cb5a653246552a875
gamesilva <gabrielmendes96@hotmail.com>
2024-06-11 22:25:26 -0300

    add AuthorizeHttpClientDecorator factory

---
+++ lib/main/factories/http/authorize_http_client_decorator_factory.dart
@@ -0,0 +1,7 @@
+import '../../decorators/decorators.dart';
+import '../../factories/factories.dart';
+import '../../../data/http/http.dart';
+
+HttpClient makeAuthorizeHttpClientDecorator() => AuthorizeHttpClientDecorator(
+    decoratee: makeHttpAdapter(),
+    fetchSecureCacheStorage: makeLocalStorageAdapter());
