08120d5625f99ffd0da80ba6a0a8e1c36b9263e0
gamesilva <gabrielmendes96@hotmail.com>
2024-06-17 21:15:13 -0300

    ensure LocalLoadSurveys throws UnexpectedError if cache is null

--- test/data/usecases/load_surveys/local_load_surveys_test.dart
+++ test/data/usecases/load_surveys/local_load_surveys_test.dart
@@ -14,7 +14,7 @@ class LocalLoadSurveys {
 
   Future<List<SurveyEntity>>? load() async {
     final data = await fetchCacheStorage.fetch('surveys');
-    if (data.isEmpty) {
+    if (data?.isEmpty != false) {
       throw DomainError.unexpected;
     }
     return data
@@ -32,7 +32,7 @@ abstract class FetchCacheStorage {
 void main() {
   late FetchCacheStorage fetchCacheStorage;
   late LocalLoadSurveys sut;
-  late List<Map> data;
+  List<Map>? data;
 
   List<Map> mockValidData() => [
         {
@@ -49,7 +49,7 @@ void main() {
         },
       ];
 
-  void mockFetch(List<Map> list) {
+  void mockFetch(List<Map>? list) {
     data = list;
     when(() => fetchCacheStorage.fetch('surveys'))
         .thenAnswer((_) async => data);
@@ -75,14 +75,14 @@ void main() {
 
     expect(surveys, [
       SurveyEntity(
-        id: data[0]['id'],
-        question: data[0]['question'],
+        id: data?[0]['id'],
+        question: data?[0]['question'],
         dateTime: DateTime.utc(2020, 7, 20),
         didAnswer: false,
       ),
       SurveyEntity(
-        id: data[1]['id'],
-        question: data[1]['question'],
+        id: data?[1]['id'],
+        question: data?[1]['question'],
         dateTime: DateTime.utc(2019, 2, 2),
         didAnswer: true,
       ),
@@ -91,6 +91,13 @@ void main() {
 
   test('Should throws UnexpectedError if cache is empty', () async {
     mockFetch([]);
+    final future = sut.load();
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('Should throws UnexpectedError if cache is null', () async {
+    mockFetch(null);
     final future = sut.load();
 
     expect(future, throwsA(DomainError.unexpected));
