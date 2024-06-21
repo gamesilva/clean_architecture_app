23d2595fed35745b15899fef9569b301cfd0b493
gamesilva <gabrielmendes96@hotmail.com>
2024-06-21 10:16:44 -0300

    ensure LocalLoadSurveys calls CacheStorage.save with correct values

--- lib/data/cache/cache_storage.dart
+++ lib/data/cache/cache_storage.dart
@@ -1,4 +1,5 @@
 abstract class CacheStorage {
   Future<dynamic>? fetch(String key) async {}
   Future<void>? delete(String key) async {}
+  Future<void>? save({required String key, required dynamic value}) async {}
 }
