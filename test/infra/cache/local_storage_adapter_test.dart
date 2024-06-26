d06f9f89bf807e479af5ec8a6cf7a5c29207b8b1
gamesilva <gabrielmendes96@hotmail.com>
2024-06-25 21:54:21 -0300

    move sut creation to the setup method

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -16,13 +16,21 @@ class LocalStorageAdapter {
 class LocalStorageSpy extends Mock implements LocalStorage {}
 
 void main() {
+  late String key;
+  late dynamic value;
+
+  late LocalStorage localStorage;
+  late LocalStorageAdapter sut;
+
+  setUp(() {
+    key = faker.randomGenerator.string(5);
+    value = faker.randomGenerator.string(50);
+
+    localStorage = LocalStorageSpy();
+    sut = LocalStorageAdapter(localStorage: localStorage);
+  });
+
   test('Should call localStorage with correct values', () async {
-    final key = faker.randomGenerator.string(5);
-    final value = faker.randomGenerator.string(50);
-
-    final localStorage = LocalStorageSpy();
-    final sut = LocalStorageAdapter(localStorage: localStorage);
-
     await sut.save(key: key, value: value);
 
     verify(() => localStorage.setItem(key, value)).called(1);
