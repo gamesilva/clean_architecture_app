17e967631c5c286e25b2585602180ce7b990cbd8
gamesilva <gabrielmendes96@hotmail.com>
2024-06-25 21:51:56 -0300

    ensure LocalStorageAdapter calls LocalStorage with correct values

--- lib/data/cache/cache_storage.dart
+++ lib/data/cache/cache_storage.dart
@@ -1,5 +1,5 @@
 abstract class CacheStorage {
-  Future<dynamic>? fetch(String key) async {}
-  Future<void>? delete(String key) async {}
-  Future<void>? save({required String key, required dynamic value}) async {}
+  Future<dynamic>? fetch(String key);
+  Future<void>? delete(String key);
+  Future<void>? save({required String key, required dynamic value});
 }
