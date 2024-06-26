79a4ebe454475bd1f584266e8ec74abb521c841d
gamesilva <gabrielmendes96@hotmail.com>
2024-06-25 22:25:17 -0300

    ensure LocalStorageAdapter returns same value as cache on fetch

--- lib/infra/cache/local_storage_adapter.dart
+++ lib/infra/cache/local_storage_adapter.dart
@@ -1,20 +1,24 @@
+import 'package:clean_architecture_app/data/cache/cache.dart';
 import 'package:localstorage/localstorage.dart';
 
-class LocalStorageAdapter {
+class LocalStorageAdapter implements CacheStorage {
   final LocalStorage localStorage;
 
   LocalStorageAdapter({required this.localStorage});
 
+  @override
   Future<void>? save({required String key, required dynamic value}) async {
     await localStorage.deleteItem(key);
     await localStorage.setItem(key, value);
   }
 
+  @override
   Future<void>? delete(String key) async {
     await localStorage.deleteItem(key);
   }
 
-  Future<void>? fetch(String key) async {
+  @override
+  Future<dynamic>? fetch(String key) async {
     await localStorage.getItem(key);
   }
 }
