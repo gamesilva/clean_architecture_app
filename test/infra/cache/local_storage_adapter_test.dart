2f20d18aa3908be3d435fa35f239423cc99cebec
gamesilva <gabrielmendes96@hotmail.com>
2024-06-25 22:08:27 -0300

    improve mock method names

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -14,10 +14,10 @@ void main() {
   late LocalStorage localStorage;
   late LocalStorageAdapter sut;
 
-  void mockDeleteItemError() =>
+  void mockDeleteError() =>
       when(() => localStorage.deleteItem(any())).thenThrow(Exception());
 
-  void mockSetItemError() =>
+  void mockSaveError() =>
       when(() => localStorage.setItem(any(), any())).thenThrow(Exception());
 
   setUp(() {
@@ -36,14 +36,14 @@ void main() {
   });
 
   test('Should throw if deleteItem throws', () async {
-    mockDeleteItemError();
+    mockDeleteError();
     final future = sut.save(key: key, value: value);
 
     expect(future, throwsA(const TypeMatcher<Exception>()));
   });
 
   test('Should throw if setItem throws', () async {
-    mockSetItemError();
+    mockSaveError();
     final future = sut.save(key: key, value: value);
 
     expect(future, throwsA(const TypeMatcher<Exception>()));
