458bc90d70857585527f203cc6931d05f8875db9
gamesilva <gabrielmendes96@hotmail.com>
2024-06-26 12:38:51 -0300

    ensure Composite returns remote data

--- test/main/composites/remote_load_surveys_with_local_fallback_test.dart
+++ test/main/composites/remote_load_surveys_with_local_fallback_test.dart
@@ -4,8 +4,9 @@ import 'package:test/test.dart';
 
 import 'package:clean_architecture_app/data/usecases/usecases.dart';
 import 'package:clean_architecture_app/domain/entities/entities.dart';
+import 'package:clean_architecture_app/domain/usecases/usecases.dart';
 
-class RemoteLoadSurveysWithLocalFallback {
+class RemoteLoadSurveysWithLocalFallback implements LoadSurveys {
   final RemoteLoadSurveys remote;
   final LocalLoadSurveys local;
 
@@ -14,9 +15,11 @@ class RemoteLoadSurveysWithLocalFallback {
     required this.local,
   });
 
-  Future<void>? load() async {
+  @override
+  Future<List<SurveyEntity>?>? load() async {
     final surveys = await remote.load();
     await local.save(surveys);
+    return surveys;
   }
 }
 
@@ -28,7 +31,7 @@ void main() {
   late RemoteLoadSurveys remote;
   late LocalLoadSurveys local;
   late RemoteLoadSurveysWithLocalFallback sut;
-  late List<SurveyEntity> surveys;
+  late List<SurveyEntity> remoteSurveys;
 
   List<SurveyEntity> mockSurveys() => [
         SurveyEntity(
@@ -40,8 +43,8 @@ void main() {
       ];
 
   void mockRemoteLoad() {
-    surveys = mockSurveys();
-    when(() => remote.load()).thenAnswer((_) async => surveys);
+    remoteSurveys = mockSurveys();
+    when(() => remote.load()).thenAnswer((_) async => remoteSurveys);
   }
 
   setUp(() {
@@ -61,6 +64,12 @@ void main() {
   test('Should call local save with remote data', () async {
     await sut.load();
 
-    verify(() => local.save(surveys)).called(1);
+    verify(() => local.save(remoteSurveys)).called(1);
+  });
+
+  test('Should return remote data', () async {
+    final surveys = await sut.load();
+
+    expect(surveys, remoteSurveys);
   });
 }
