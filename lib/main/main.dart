1862c1dd2579f7991c20e0b4c63f854f3b474ad8
gamesilva <gabrielmendes96@hotmail.com>
2024-07-01 20:41:44 -0300

    add SurveyResult Page

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -20,7 +20,7 @@ class App extends StatelessWidget {
       title: '4Dev',
       debugShowCheckedModeBanner: false,
       theme: makeAppTheme(),
-      initialRoute: '/',
+      initialRoute: '/survey_result/3',
       getPages: [
         GetPage(
           name: '/',
@@ -40,6 +40,10 @@ class App extends StatelessWidget {
           name: '/surveys',
           page: makeSurveysPage,
           transition: Transition.fadeIn,
+        ),
+        GetPage(
+          name: '/survey_result/:survey_id',
+          page: makeSurveyResultPage,
         ),
       ],
     );
