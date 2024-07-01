b15a26080d4f775372581c9eb6dcc22a79732aa2
gamesilva <gabrielmendes96@hotmail.com>
2024-07-01 19:39:30 -0300

    add SurveyResultEntity

---
+++ lib/domain/entities/survey_result_entity.dart
@@ -0,0 +1,13 @@
+import './entities.dart';
+
+class SurveyResultEntity {
+  final String? surveyId;
+  final String? question;
+  final List<SurveyAnswerEntity>? answers;
+
+  SurveyResultEntity({
+    required this.surveyId,
+    required this.question,
+    required this.answers,
+  });
+}
