5fffc99c9f79b373b52b9fad18c4d6e9e2c2012d
gamesilva <gabrielmendes96@hotmail.com>
2024-07-01 21:15:09 -0300

    add SurveyResult Layout

--- lib/ui/components/app_theme.dart
+++ lib/ui/components/app_theme.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
 ThemeData makeAppTheme() {
+  final disabledColor = Colors.grey[400];
+  const dividerColor = Colors.grey;
   const primaryColor = Color.fromRGBO(136, 14, 79, 1);
   const primaryColorDark = Color.fromRGBO(96, 0, 38, 1);
   const primaryColorLight = Color.fromRGBO(188, 71, 123, 1);
   const secondaryColorDark = Color.fromRGBO(0, 37, 26, 1);
+  const secondaryColor = Color.fromRGBO(0, 77, 64, 1);
   const textTheme = TextTheme(
     headline1: TextStyle(
       fontSize: 30,
@@ -38,7 +41,10 @@ ThemeData makeAppTheme() {
     primaryColor: primaryColor,
     primaryColorDark: primaryColorDark,
     primaryColorLight: primaryColorLight,
+    highlightColor: secondaryColor,
     secondaryHeaderColor: secondaryColorDark,
+    disabledColor: disabledColor,
+    dividerColor: dividerColor,
     backgroundColor: Colors.white,
     textTheme: textTheme,
     inputDecorationTheme: inputDecorationTheme,
