0a7682e0cfb10adf5b54c54570172d2cd58da9bd
gamesilva <gabrielmendes96@hotmail.com>
2024-07-04 20:09:34 -0300

    move SurveyResult Component to its own file

--- lib/ui/pages/survey_result/survey_result_page.dart
+++ lib/ui/pages/survey_result/survey_result_page.dart
@@ -37,71 +37,7 @@ class SurveyResultPage extends StatelessWidget {
               }
 
               if (snapshot.hasData) {
-                return ListView.builder(
-                  itemCount: 4,
-                  itemBuilder: (context, index) {
-                    if (index == 0) {
-                      return Container(
-                        padding: const EdgeInsets.only(
-                          top: 40,
-                          bottom: 20,
-                          left: 20,
-                          right: 20,
-                        ),
-                        child: const Text('Qual é seu framework web favorito?'),
-                        decoration: BoxDecoration(
-                          color: Theme.of(context).disabledColor.withAlpha(90),
-                        ),
-                      );
-                    }
-
-                    return Column(
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.all(15),
-                          decoration: BoxDecoration(
-                            color: Theme.of(context).backgroundColor,
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Image.network(
-                                // 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcR5U16C8yXgBpl7-Bc7Itjx3_LRl425zINA&s', // React
-                                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3qAb3e9auwnxJ1RmaypTdsieWkl2wb4u3cg&s', // Angular
-                                width: 40,
-                              ),
-                              const Expanded(
-                                child: Padding(
-                                  padding: EdgeInsets.symmetric(horizontal: 10),
-                                  child: Text(
-                                    'Angular',
-                                    style: TextStyle(fontSize: 16),
-                                  ),
-                                ),
-                              ),
-                              Text(
-                                '100%',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                  color: Theme.of(context).primaryColorDark,
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(left: 10),
-                                child: Icon(
-                                  Icons.check_circle,
-                                  color: Theme.of(context).highlightColor,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        const Divider(height: 1)
-                      ],
-                    );
-                  },
-                );
+                return const SurveyResult();
               }
               return const SizedBox(height: 0);
             },
