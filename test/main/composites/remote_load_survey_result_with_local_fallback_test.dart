8fa3c76ee439804d61126ca201d44841e7ede255
gamesilva <gabrielmendes96@hotmail.com>
2024-07-12 16:57:48 -0300

    ensure Composite calls local save with remote data

--- test/main/composites/remote_load_survey_result_with_local_fallback_test.dart
+++ test/main/composites/remote_load_survey_result_with_local_fallback_test.dart
@@ -3,33 +3,71 @@ import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
 import 'package:clean_architecture_app/data/usecases/usecases.dart';
+import 'package:clean_architecture_app/domain/entities/entities.dart';
 
 class RemoteLoadSurveyResultWithLocalFallback {
   final RemoteLoadSurveyResult remote;
+  final LocalLoadSurveyResult local;
 
-  RemoteLoadSurveyResultWithLocalFallback({required this.remote});
+  RemoteLoadSurveyResultWithLocalFallback({
+    required this.remote,
+    required this.local,
+  });
   Future<void>? loadBySurvey({required String surveyId}) async {
-    await remote.loadBySurvey(surveyId: surveyId);
+    final surveyResult = await remote.loadBySurvey(surveyId: surveyId);
+    await local.save(surveyId: surveyId, surveyResult: surveyResult);
   }
 }
 
 class RemoteLoadSurveyResultSpy extends Mock implements RemoteLoadSurveyResult {
 }
 
+class LocalLoadSurveyResultSpy extends Mock implements LocalLoadSurveyResult {}
+
 void main() {
   late String surveyId;
   late RemoteLoadSurveyResult remote;
+  late LocalLoadSurveyResult local;
   late RemoteLoadSurveyResultWithLocalFallback sut;
+  late SurveyResultEntity surveyResult;
+
+  void mockSurveyResult() {
+    surveyResult = SurveyResultEntity(
+      surveyId: surveyId,
+      question: faker.lorem.sentence(),
+      answers: [
+        SurveyAnswerEntity(
+          answer: faker.lorem.sentence(),
+          isCurrentAnswer: faker.randomGenerator.boolean(),
+          percent: faker.randomGenerator.integer(100),
+        ),
+      ],
+    );
+
+    when(() => remote.loadBySurvey(surveyId: any(named: 'surveyId')))
+        .thenAnswer((_) async => surveyResult);
+  }
 
   setUp(() {
     surveyId = faker.guid.guid();
     remote = RemoteLoadSurveyResultSpy();
-    sut = RemoteLoadSurveyResultWithLocalFallback(remote: remote);
+    local = LocalLoadSurveyResultSpy();
+    sut = RemoteLoadSurveyResultWithLocalFallback(
+      remote: remote,
+      local: local,
+    );
+    mockSurveyResult();
   });
 
   test('Should call remote LoadBySurvey', () async {
     await sut.loadBySurvey(surveyId: surveyId);
 
     verify(() => remote.loadBySurvey(surveyId: surveyId));
+  });
+
+  test('Should call local save with remote data', () async {
+    await sut.loadBySurvey(surveyId: surveyId);
+
+    verify(() => local.save(surveyId: surveyId, surveyResult: surveyResult));
   });
 }
